0412e78718f635fc0db2c3c676d9e68394550059
MateusKekW <mateusacdem@gmail.com>
2024-06-04 08:48:03 -0300

    commit modulos e pacotes

---
+++ 03-modulos-pacotes/main.py
@@ -0,0 +1,44 @@
+#importa tudo do pacote
+import matematica
+ #from matematica import * 
+
+#importa algo específico
+from matematica import somar, PI
+#from matematica import somar as somar_mat, PI as PI_mat
+
+print(matematica.PI)
+print(matematica.somar(10.0, 3.0))
+
+print(PI)
+print(somar(10.0, 3.0))
+
+from estatistica.descritiva import *
+from estatistica.inferencial import VALOR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03-modulos-pacotes/matematica.py
@@ -0,0 +1,4 @@
+PI = 3.14159265359
+
+def somar(*numeros):
+    return sum(numeros)
